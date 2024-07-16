2cd451874effc967c5f975b94e43bbe29f449bbb
SinmisolaE <sinmisolaajao@gmail.com>
2024-07-16 14:30:31 -0700

    101-students.py

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -15,7 +15,7 @@ def top_students(mongo_collection):
         {
             "$project":
                 {"name": "$name",
-                    "averageScore": {"avg": "$topics.score"}}
+                 "averageScore": {"$avg": "$topics.score"}}
         },
         {
             "$sort":
